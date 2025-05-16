af0b6bf482dc2f1328fcf9bf80a9d125fb34f432
Fermin Regidor <ferminregidor2005@gmail.com>
2025-05-16 00:50:06 -0300

    Add files via upload

---
+++ Viajes/Viajes/Class1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace Viajes
+{
+    internal class CViajes
+    {
+        private float precio_kilometraje; // precio por kilometro recorrido
+        private ushort kilometraje_minimo; // kilometraje minimo de cada viaje con el que si el viaje fue menor a este minimo, se le cobrara lo minimo en kilometro * precio de kilometraje
+        private string dominio; // patente del viaje
+        private ushort kilometraje_recorrido; // kilometros recorridos
+        private ushort peajes; // la suma total de los peajes cobrados
+
+
+        public CViajes()
+        {
+            precio_kilometraje = 0;
+            kilometraje_minimo = 0;
+            dominio = " ";
+            kilometraje_recorrido = 0;
+        }
+        public CViajes(float precio_kilometraje, ushort kilometraje_minimo, string dominio, ushort kilometraje_recorrido, ushort peajes)
+        {
+            this.precio_kilometraje = precio_kilometraje;
+            this.kilometraje_minimo = kilometraje_minimo;
+            this.dominio = dominio;
+            this.kilometraje_recorrido = kilometraje_recorrido;
+            this.peajes = peajes;
+        }
+        // Retornos
+        public float RetornoPrecioKilometraje()
+        {
+            return precio_kilometraje;
+        } 
+        public ushort RetornoKilometrajeMinimo()
+        {
+            return kilometraje_minimo;
+        }
+        public string RetornoDominio()
+        {
+            return dominio;
+        }
+        public ushort RetornoKilometrajeRecorrido()
+        {
+            return kilometraje_recorrido;
+        }
+        public ushort RetornoPeajes()
+        {
+            return peajes;
+        }
+
+        // Precios
+        public float precio_viaje_sin_peajes()
+        {
+            if(kilometraje_recorrido < kilometraje_minimo)
+            {
+                return precio_kilometraje * kilometraje_minimo;
+            }
+            else
+            {
+                return precio_kilometraje * kilometraje_recorrido;
+            }
+        }
+        public float precio_viaje_con_peajes()
+        {
+            if (kilometraje_recorrido < kilometraje_minimo)
+            {
+                return (precio_kilometraje * kilometraje_minimo) + peajes;
+            }
+            else
+            {
+                return (precio_kilometraje * kilometraje_recorrido) + peajes;
+            }
+        }
+    }
+}
---
+++ Viajes/Viajes/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+namespace Viajes
+{
+    class Program
+    {
+        private static Viajes sucursal = new Viajes();
+        static void Main(string[] args)
+        {
+            Console.WriteLine(">>Inicio del programa.");
+            Console.ReadKey();
+            sucursal.Carga();
+            sucursal.Impresion();
+            sucursal.Mayor();
+        }
+    }
+    class Viajes
+    {
+        private List<CViajes> viajes = new List<CViajes>();
+        public void Carga()
+        {
+            Console.WriteLine();
+            Console.WriteLine(">>Carga de datos:");
+            Console.WriteLine("Ingrese el precio por kilometro: ");
+            float precio_kilometraje = float.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el kilometraje minimo: ");
+            ushort kilometraje_minimo = ushort.Parse(Console.ReadLine());
+            bool flagWhile = false;
+
+            do
+            {
+                Console.WriteLine("Patente del viaje (Ingresar 'FIN' si no desea ingresar mas viajes): ");
+                string dominio = Console.ReadLine();
+                if (dominio == "FIN")
+                {
+                    flagWhile = true;
+                }
+                if (string.IsNullOrEmpty(dominio))
+                {
+                    Console.WriteLine("Ingrese un valor valido...");
+                }
+                else if (flagWhile != true)
+                {
+                    Console.WriteLine("Kilometros recorridos: ");
+                    ushort kilometraje_recorrido = ushort.Parse(Console.ReadLine());
+                    Console.WriteLine("Peajes (La suma total de los peajes cobrados):");
+                    ushort peajes = ushort.Parse(Console.ReadLine());
+                    viajes.Add(new CViajes(precio_kilometraje, kilometraje_minimo, dominio, kilometraje_recorrido, peajes));
+                    Console.WriteLine("Ingreso exitoso!");
+                }
+            } while (flagWhile != true);
+        }
+        public void Impresion()
+        {
+            Console.WriteLine(">>Impresion de datos:");
+            if(viajes.Count == 0)
+            {
+                Console.WriteLine("No se ingresaron viajes validos...");
+            }
+            foreach(CViajes cvi in viajes)
+            {
+                Console.WriteLine($"Precio kilometraje: {cvi.RetornoPrecioKilometraje} - Kilometraje minimo: {cvi.RetornoKilometrajeMinimo} - Patente: {cvi.RetornoDominio} - Kilometraje recorrido: {cvi.RetornoKilometrajeRecorrido}");
+                Console.WriteLine($"Precio total del viaje (sin peajes): {cvi.precio_viaje_sin_peajes} ");
+                Console.WriteLine($"Precio total del viaje (con peajes): {cvi.precio_viaje_con_peajes} ");
+            }
+        }
+        public void Mayor()
+        {
+            string mayor = " ";
+            float mayorPrecio = 0;
+            foreach(CViajes cvi in viajes)
+            {
+                if(cvi.precio_viaje_sin_peajes() > mayorPrecio)
+                {
+                    mayorPrecio = cvi.precio_viaje_sin_peajes();
+                    mayor = cvi.RetornoDominio();
+                }
+            }
+            foreach(CViajes cvi in viajes) // sergundo foreach para buscar al mayor por la patente y llamar a su impresion
+            {
+                if(cvi.RetornoDominio() == mayor)
+                {
+                    Console.WriteLine("Impresion del viaje mas caro:");
+                    Console.WriteLine($"Precio kilometraje: {cvi.RetornoPrecioKilometraje} - Kilometraje minimo: {cvi.RetornoKilometrajeMinimo} - Patente: {cvi.RetornoDominio} - Kilometraje recorrido: {cvi.RetornoKilometrajeRecorrido}");
+                    Console.WriteLine($"Precio total del viaje (sin peajes): {cvi.precio_viaje_sin_peajes} ");
+                }
+            }
+        }
+    }
+}
